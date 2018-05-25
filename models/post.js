bd1a04cc8f7e27adf9603d6600a862229648b03b
David Thayer <david.thayer@gmail.com>
2018-05-25 15:39:51 -0700

    route and controller up

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+mongoose.connect('mongodb://localhost/post-api');
+
+let PostSchema = new Schema({
+    make: String,
+    model: String,
+    lease_price: Number,
+    purch_price: Number,
+    details: String,
+    website: String,
+    style: String,
+    transaction_type: String,
+    ev: Boolean,
+})
+
+let Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post
--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const Post = require('./models/post')
+
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+  });
 
 app.get('/',function(req, res) {
     res.sendFile('views/index.html', { root:__dirname});
 });
 
-app.listen(8080)
+app.get('/api/posts', function index(req, res) {
+    Post.find({}, function(err, posts) {
+        if (err) res.send(err);
+        else res.json(posts);
+    });
+})
+
+app.post('/api/posts', function(req, res) {
+    Post.create(req.body, function (err, post) {
+        if (err) res.send(err);
+        else res.json(post);
+    });
+});
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log(`Listening on port ${ port }`);
+});
